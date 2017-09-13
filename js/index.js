d1ad8690c0896219d7ebd1e9a5b8d3fca43ddf98
Dereje1 <Dereje1>
2017-09-13 14:57:54 -0400

    test flag sprites

--- js/index.js
+++ js/index.js
@@ -43,11 +43,82 @@ d3.json(url,function(error,forceData){//use d3's own json capabilites to get dat
     if(!sourceArr.includes(sourceVal)){sourceArr.push(sourceVal)}
     if(sourceVal===checkKey){
       let targetVal = forceData.links[i].target
-      console.log(targetVal)
-      console.log(forceData.nodes[targetVal])
+      //console.log(targetVal)
+      //console.log(forceData.nodes[targetVal])
     }
+    var simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().id(function(d) { return d.index }))
+      .force("collide",d3.forceCollide( function(d){return 8 }).iterations(16) )
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .force("y", d3.forceY(0))
+      .force("x", d3.forceX(0))
   }
-  console.log(sourceArr)
+        var link = chart.append("g")
+          .attr("class", "links")
+          .selectAll("line")
+          .data(forceData.links)
+          .enter()
+          .append("line")
+          .attr("stroke", "white")
+
+      var node = chart.append("g")
+          .attr("class", "nodes")
+          .selectAll("image")
+          .data(forceData.nodes)
+          .enter().append("svg:image")
+          .attr("xlink:href", "C:\Users\getah\github\Force-Directed\css\flags\flags.png")
+          .attr("class","flag flag-et")
+          .call(d3.drag()
+              .on("start", dragstarted)
+              .on("drag", dragged)
+              .on("end", dragended))
+          .on("mouseover", function(d) {//tool tip functionality
+             toolTipDiv.html("<strong>"+d.country +"</strong><br/>")
+               .style("left", (d3.event.pageX) + "px")
+               .style("top", (d3.event.pageY+40) + "px")
+               .style("visibility", "visible");
+             })
+           .on("mouseout", function(d) {
+             toolTipDiv.style("visibility", "hidden");
+             });
+
+      var ticked = function() {
+          link
+              .attr("x1", function(d) { return d.source.x; })
+              .attr("y1", function(d) { return d.source.y; })
+              .attr("x2", function(d) { return d.target.x; })
+              .attr("y2", function(d) { return d.target.y; });
+
+          node
+              .attr("cx", function(d) { return d.x; })
+              .attr("cy", function(d) { return d.y; });
+      }
+
+      simulation
+        .nodes(forceData.nodes)
+        .on("tick", ticked);
+
+      simulation.force("link")
+        .links(forceData.links);
+
+      function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+      }
+
+      function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+      }
+
+      function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+      }
+  //console.log(sourceArr)
   console.log(forceData)
 
 })
