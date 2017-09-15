d326cc34ed83f4a20503024517a42a1148f71f28
Dereje1 <Dereje1>
2017-09-14 21:45:15 -0400

    finsihed w/o commenting + cleanup

--- js/index.js
+++ js/index.js
@@ -1,21 +1,10 @@
 const url = "https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json"
-const margin = {top: 100, right: 90, bottom: 25, left: 75}
+const margin = {top: 0, right: 0, bottom: 5, left: 0}
 let width = window.innerWidth*.85-margin.left-margin.right,
     height = window.innerHeight*.85-margin.top-margin.bottom;
 
 
-let x = d3.scaleLinear()//use linear for x since only year data given
-    .range([0,width]);
-let y = d3.scaleLinear()//use linear for y since only months are used
-    .range([0,height]);
 
-let x2=d3.scaleTime()//used for axis display only
-    .rangeRound([0, width]);
-let y2=d3.scaleTime()//used for axis display only
-    .range([0, height])
-let xAxis = d3.axisBottom(x2)//xaxis display properties
-    .ticks(10)
-let yAxis = d3.axisLeft(y2)//yaxis display properties
 let toolTipDiv = d3.select("body").append("div")//toolTip div definition, definition in css sheet would not work for me???
             .attr("class", "toolTip")
             .style("position", "absolute")
@@ -27,6 +16,7 @@ let toolTipDiv = d3.select("body").append("div")//toolTip div definition, defini
             .style("text-align", "center")
             .style("visibility", "hidden");
 
+
 let chart = d3.select(".chart")//main chart definition
     .attr("width", width + margin.left + margin.right)//margins added for axis
     .attr("height", height + margin.top + margin.bottom)
@@ -36,45 +26,40 @@ let chart = d3.select(".chart")//main chart definition
 d3.json(url,function(error,forceData){//use d3's own json capabilites to get data
   if (error) throw error;
 
-  const checkKey =9
-  let sourceArr=[]
-  for(let i=0;i<forceData.links.length;i++){
-    let sourceVal = forceData.links[i].source
-    if(!sourceArr.includes(sourceVal)){sourceArr.push(sourceVal)}
-    if(sourceVal===checkKey){
-      let targetVal = forceData.links[i].target
-      //console.log(targetVal)
-      //console.log(forceData.nodes[targetVal])
-    }
     var simulation = d3.forceSimulation()
-      .force("link", d3.forceLink().id(function(d) { return d.index }))
+      .force("link", d3.forceLink().id(function(d) {return d.index }))
       .force("collide",d3.forceCollide( function(d){return 8 }).iterations(16) )
       .force("charge", d3.forceManyBody())
       .force("center", d3.forceCenter(width / 2, height / 2))
       .force("y", d3.forceY(0))
       .force("x", d3.forceX(0))
-  }
+
         var link = chart.append("g")
           .attr("class", "links")
           .selectAll("line")
           .data(forceData.links)
           .enter()
           .append("line")
-          .attr("stroke", "white")
-
-      var node = chart.append("g")
-          .attr("class", "nodes")
-          .selectAll("image")
+          .on("mouseover", function(d) {//tool tip functionality
+             toolTipDiv.html("<strong>"+d.country +"</strong><br/>" +(d3.event.pageY))
+               .style("left", (d3.event.pageX) + "px")
+               .style("top", (d3.event.pageY+40) + "px")
+               .style("visibility", "visible");
+             })
+           .on("mouseout", function(d) {
+             toolTipDiv.style("visibility", "hidden");
+             });
+      var node = d3.select("#flags")
+          .selectAll("img")
           .data(forceData.nodes)
-          .enter().append("svg:image")
-          .attr("xlink:href", "C:\Users\getah\github\Force-Directed\css\flags\flags.png")
-          .attr("class","flag flag-et")
+          .enter().append("img")
+          .attr("class",function(d){return ("flag flag-"+d.code)})
           .call(d3.drag()
               .on("start", dragstarted)
               .on("drag", dragged)
               .on("end", dragended))
           .on("mouseover", function(d) {//tool tip functionality
-             toolTipDiv.html("<strong>"+d.country +"</strong><br/>")
+             toolTipDiv.html("<strong>"+d.country +"</strong><br/>" +(d3.event.pageY))
                .style("left", (d3.event.pageX) + "px")
                .style("top", (d3.event.pageY+40) + "px")
                .style("visibility", "visible");
@@ -84,6 +69,7 @@ d3.json(url,function(error,forceData){//use d3's own json capabilites to get dat
              });
 
       var ticked = function() {
+
           link
               .attr("x1", function(d) { return d.source.x; })
               .attr("y1", function(d) { return d.source.y; })
@@ -91,8 +77,8 @@ d3.json(url,function(error,forceData){//use d3's own json capabilites to get dat
               .attr("y2", function(d) { return d.target.y; });
 
           node
-              .attr("cx", function(d) { return d.x; })
-              .attr("cy", function(d) { return d.y; });
+              .style("left", function(d) { return (d.x+margin.left)+'px' })
+              .style("top", function(d) { return (d.y+margin.top)+'px' });
       }
 
       simulation
@@ -122,3 +108,14 @@ d3.json(url,function(error,forceData){//use d3's own json capabilites to get dat
   console.log(forceData)
 
 })
+
+function flagTip(countryInfo){
+  //console.log((countryInfo))
+  let flagClass = "flag flag-"+countryInfo.code
+  d3.select("#flagPlaceHolder")
+              .append("div")
+              .attr("class", flagClass)
+              .style("position", "absolute")
+              .style("visibility", "visible");
+
+}
